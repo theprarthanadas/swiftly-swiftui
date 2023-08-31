a4afe2632952579c36046b5a306168cf2e89398d
theprarthanadas <55353305+theprarthanadas@users.noreply.github.com>
2023-09-01 00:09:45 +0530

    Creating custom paths with SwiftUI

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Prarthana Das on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
